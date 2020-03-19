fb364e326c4dbef59d98a5a7aa836e0c860d332d
Vlad <vlados.pochta@gmail.com>
2020-03-19 16:00:20 +0300

    v3.0.0
    Начало работы с Mongodb

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-import * as pars from './parser_pokemons';
+import mongoose from 'mongoose';
+import Promise from 'bluebird';
+
+mongoose.Promise = Promise;
+mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const Pet = mongoose.model('Pet', { 
+    type: String,
+    name: String,
+});
+
+const kitty = new Pet({ 
+    name: 'Zildjian',
+    type: 'cat',
+});
+
+kitty.save()
+.then(() => {
+    console.log('success');
+})
+.catch((err) => {
+    console.log('err', err);
+});
