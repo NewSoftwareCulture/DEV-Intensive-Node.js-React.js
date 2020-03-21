5854536871780d1aa0eb24abd14edb555f9f32c5
Vlad <vlados.pochta@gmail.com>
2020-03-21 03:18:16 +0300

    v3.1.0

--- src/index.js
+++ src/index.js
@@ -1,23 +1,42 @@
+import express from 'express';
+import cors from 'cors';
 import mongoose from 'mongoose';
 import Promise from 'bluebird';
+import bodyParser from 'body-parser';
+import saveDataInDb from './saveDataInDb';
+import Pet from './models/Pet';
+import User from './models/User';
 
 mongoose.Promise = Promise;
-mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/vdenisov', {useNewUrlParser: true, useUnifiedTopology: true});
 
-const Pet = mongoose.model('Pet', { 
-    type: String,
-    name: String,
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/users', async(req, res) =>{
+    const users = await User.find();
+    return res.json(users);
 });
 
-const kitty = new Pet({ 
-    name: 'Zildjian',
-    type: 'cat',
+app.get('/pets', async(req, res) =>{
+    const pets = await Pet.find().populate('owner');
+    return res.json(pets);
 });
 
-kitty.save()
-.then(() => {
-    console.log('success');
-})
-.catch((err) => {
-    console.log('err', err);
+app.post('/data', async(req, res) => {
+    const data = req.body;
+    if(!data.user) return res.status(400).send('user required!');
+    if(!data.pet) data.pets = [];
+
+    try {
+        const result = await saveDataInDb(data);
+        return res.json(result);
+    } catch (error) {
+        return res.status(500).json(error);
+    }
+});
+
+app.listen(3000, function() {
+    console.log('Example app listening on port 3000!');
 });
